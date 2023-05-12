0e04b375c2eddca35959d162bdcc2698d5b5c466
ANKIT MISHRA <ankitmrmishra1118@gmail.com>
2023-05-12 18:23:17 +0530

    add: header section

---
+++ src/header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './css/header.css'
+function header() {
+  return (
+    <div className='Header--div'>
+
+<div className="paragraph--div">
+    <span className='hey'>Hey!</span> <br></br>
+    <span>"I'm Passionate about crafting websites that not only look great but also deliver a seamless user experience."
+</span>
+</div>
+<div className="myimage">
+    <img src="./src/assets/WhatsApp Image 2023-05-12 at 17.33.17.jpg" alt=""  />
+</div>
+
+
+    </div>
+  )
+}
+
+export default header
